31f2dc05d815547781aa36a1d042188ad75d7d35
Kim Dang <kimdang229@gmail.com>
2020-04-27 15:23:25 -0700

    Implement getElementsByClassName recursively

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// returns a collection of all elements in doc with sepcific name
+var getElementsByClassName = function(className) {
+  let elements = [];
+
+  // checks if element has a class list and if element has children
+  function hasClassOrChild(element) {
+    if(element.classList){
+      // case: has multiple classes
+      if (element.classList.contains(className)) {
+        elements.push(element);
+      }
+    }
+
+    if(element.hasChildNodes()) {
+      let children = element.childNodes;
+      for(const child of children) {
+        hasClassOrChild(child);
+      }
+    }
+  }
+
+  let root = document.body;
+  hasClassOrChild(root);
+
+  return elements;
 };
+//can't use '===' because it checks if both arrays are stored at the same address
+// so will always return false
