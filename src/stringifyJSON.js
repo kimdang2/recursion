0854cc8f078043e8c4e39637b559b34d247352d6
Kim Dang <kimdang229@gmail.com>
2020-04-27 22:18:55 -0700

    Implement JSON.stringify function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  let objStringify = [];
+  let arrStringify = [];
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null || obj instanceof Date) {
+    return `${obj}`
+  } else if (typeof obj === 'string'){
+      return `"${obj}"`;
+  }
+
+  else if (Array.isArray(obj)){
+    for(var ele of obj){
+      if (typeof ele === 'undefined' || typeof ele === 'function'){
+        arrStringify.push(null);
+      } else {
+        arrStringify.push(stringifyJSON(ele));
+      }
+    }
+    return `[${arrStringify}]`;
+
+  } else if (typeof obj === 'object'){
+    Object.keys(obj).forEach(function(key) { // ['name','age','color']
+      var value = obj[key];
+      if (typeof value === "function" || value === undefined){
+        delete obj[key];
+      } else if (typeof value === 'boolean' || typeof value === 'number' || value === null){
+        objStringify.push(`"${key}":${value}`);
+      } else if (typeof value === 'string'){
+        objStringify.push(`"${key}":"${value}"`); // "{"name":"value"}"
+      } else if (typeof value === "object") {
+        objStringify.push(`"${key}":${stringifyJSON(value)}`);
+      }
+    });
+    return `{${objStringify}}`;
+  }
+}
